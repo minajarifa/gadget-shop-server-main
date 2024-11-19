c6bb02c6aade303f6d0dd57c9c32b9f217cc10b4
Your Name <minajmiha02@gmail.com>
2024-11-19 08:36:24 +0600

    added .env file

--- index.js
+++ index.js
@@ -1,8 +1,11 @@
-const express = require('express');
+// git add .
+// git commit -m " added provateRout Dashboard"
+//  git push
+const express = require("express");
 const cors = require("cors");
-const jwt = require('jsonwebtoken');
+const jwt = require("jsonwebtoken");
 require("dotenv").config();
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion } = require("mongodb");
 const app = express();
 const port = process.env.PORT || 3000;
 // middleware
@@ -10,11 +13,9 @@ app.use(cors());
 app.use(express.json());
 // mongodb
 
-
-
 // const  uri = "mongodb+srv://GADGET:DWXqcOaDqMD0w5b0@cluster0.63qrdth.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0";
 
-const  uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.63qrdth.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.63qrdth.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
 const client = new MongoClient(uri, {
@@ -22,16 +23,31 @@ const client = new MongoClient(uri, {
     version: ServerApiVersion.v1,
     strict: true,
     deprecationErrors: true,
-  }
+  },
 });
+const userCollection = client.db("gadgetShop").collection("users");
+const productsCollection = client.db("gadgetShop").collection("products");
 
 async function run() {
   try {
     // Connect the client to the server	(optional starting in v4.7)
     await client.connect();
+    //
+    app.post("/users", async (req, res) => {
+      const user = req.body;
+      const query ={email: user.email};
+      const existEmail = await userCollection.findOne(query);
+      if(existEmail){
+        return res.send({message:"user already exist"})
+      }
+      const result = await userCollection.insertOne(user);
+      res.send(result);
+    });
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
-    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+    console.log(
+      "Pinged your deployment. You successfully connected to MongoDB!"
+    );
   } finally {
     // Ensures that the client will close when you finish/error
     // await client.close();
@@ -39,17 +55,18 @@ async function run() {
 }
 run().catch(console.dir);
 
-
 // api
-app.get("/",(req,res)=>{
-    res.send("server is running by gadget shop")
+app.get("/", (req, res) => {
+  res.send("server is running by gadget shop");
 });
 // jwt
-app.post('/authentication',(req,res)=>{
+app.post("/authentication", (req, res) => {
   const useEmail = req.body;
-  const token = jwt.sign(useEmail,process.env.ACCESS_KEY_TOKEN,{expiresIn:"10d"})
-  res.send({token});
-})
-app.listen(port,()=>{
-    console.log(`server is running by gadget shop on port, ${port}`)
-})
+  const token = jwt.sign(useEmail, process.env.ACCESS_KEY_TOKEN, {
+    expiresIn: "10d",
+  });
+  res.send({ token });
+});
+app.listen(port, () => {
+  console.log(`server is running by gadget shop on port, ${port}`);
+});
